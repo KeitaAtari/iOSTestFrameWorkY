41998693210a505b327733e1b0cda1341c490206
FsiKeitaAtari <Keita.Atari@sony.com>
2018-04-12 09:32:53 +0900

    テスト用のログを出力するサンプルFrameWork

---
+++ iOSTestFrameWorkY/iOSTestFrameWorkY/iOSTestFrameWorkY.h
@@ -0,0 +1,19 @@
+//
+//  iOSTestFrameWorkY.h
+//  iOSTestFrameWorkY
+//
+//  Created by AtariKeita on 2018/04/12.
+//  Copyright © 2018年 AtariKeita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSTestFrameWorkY.
+FOUNDATION_EXPORT double iOSTestFrameWorkYVersionNumber;
+
+//! Project version string for iOSTestFrameWorkY.
+FOUNDATION_EXPORT const unsigned char iOSTestFrameWorkYVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSTestFrameWorkY/PublicHeader.h>
+
+
---
+++ iOSTestFrameWorkY/iOSTestFrameWorkY/iOSTestFrameWorkY.swift
@@ -0,0 +1,14 @@
+//
+//  iOSTestFrameWorkY.swift
+//  iOSTestFrameWorkY
+//
+//  Created by AtariKeita on 2018/04/12.
+//  Copyright © 2018年 AtariKeita. All rights reserved.
+//
+import Foundation
+public class iOSTestFrameWorkY: NSObject {
+    public func iOSTestFrameWorkYMethod() {
+        print("iOSTestFrameWorkY().iOSTestFrameWorkYMethod is called !!")
+    }
+}
+
